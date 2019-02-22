4a697a8c56c60580e59e141cbbe876a725008a82
EbanksRobert <47126792+EbanksRobert@users.noreply.github.com>
2019-02-21 20:00:32 -0500

    Add files via upload

---
+++ I8_PrimeFunc_Ebanks.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 21 18:38:54 2019
+
+@author: robbi
+"""
+
+n=100
+m=2
+v=[]
+h=[]
+p=1
+def prime_check(x):
+    l=[]
+    for i in range(2,x):
+        if x/i==int(x/i):
+            l.append(x)
+    if v==l:
+        return(True)
+    else:   
+        return(False)
+
+while n>len(h):
+    p=p+1
+    if prime_check(p)==True:
+        h.append(p)
+print('Hey the',n,'prime is', h[-1:])
